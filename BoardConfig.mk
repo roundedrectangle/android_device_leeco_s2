fbec7bc8099f25b3573541505c276ff7e80b9ed3
roundedrectangle <roundedrectangle0@gmail.com>
2025-09-14 08:59:30 +0300

    enable selinux
    Change-Id: Ic183e6b0d8c7810724278e82e9ec75892fe3c8cc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk selinux=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
